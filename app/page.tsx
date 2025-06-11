345939ded00fe8fa2d36f43c5b36be2128972c0d
g0dlessjs <juangalarce.dev@gmail.com>
2025-06-10 22:23:45 -0400

    Implement Card Companion Design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
 
-export default function Home() {
+const page = () => {
   return (
-    <div>
-      <h1 className="text-5xl underline">Hola Mundo</h1>
-      <Button>Enviar</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neura the Brainy Explorer"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivates y Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList />
+        <CTA />
+      </section>
+    </main>
   );
-}
+};
+
+export default page;
---
+++ components/CompanionList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CompanionList = () => {
+  return (
+    <div>
+      <h1>Companion List</h1>
+    </div>
+  );
+};
+
+export default CompanionList;
